aee6b7bf12bcd16b1f1c122a2c12a49eb244feab
Giattino <40548@nelnet.net>
2021-06-04 16:41:17 -0400

    adding changes for test 1 - failing

--- src/MortgageLender.ts
+++ src/MortgageLender.ts
@@ -1,1 +1,24 @@
-export default class MortgageLender {}
+export default class MortgageLender {
+    availableFunds:number = 0;
+    private _addFunds: number = 0;
+    constructor() {
+    }
+
+    public get addFunds() {
+        console.log(this._addFunds);
+        return this._addFunds;
+
+    }
+
+    public set addFunds(depositAmt: number){
+        this._addFunds = depositAmt;
+    }
+            
+    addFunds(_addFunds: number):number {
+      this._availableFunds += this._addFunds;
+      return this._availableFunds;
+    }
+
+
+}
+
